f4b4f9c0d64cc72360fee0a0de28e9a4cdbf7457
AC <msashvegas@gmail.com>
2017-11-17 05:10:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ class BoardMember {
   this.homeState = homeState;
   this.training = training;
   }
-}
 
-veto () {
-  return 'No, I must disagree.'
+  veto () {
+    return 'No, I must disagree.'
+  }
 }
